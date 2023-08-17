0f15517eacce36f8a46ad29f7ea4e2705e7aa584
vishal sawai <vishalsawai1725@gmail.com>
2023-08-17 22:17:17 +0530

    ok

--- src/components/Header.js
+++ src/components/Header.js
@@ -29,25 +29,27 @@ const Header = () => {
     const [isTargetVisible, setIsTargetVisible] = useState(false);
 
     const mainNav = document.querySelector('.HeaderContainer');
-    // const navdata = document.querySelector('.navHeader');
     const handleMouseEnter = () => {
         setIsTargetVisible(true);
         mainNav.style.backgroundColor = 'White';
-        // navdata.style.color = 'black !important';
+        setNavbar(true);
     };
 
     const handleMouseLeave = () => {
         setIsTargetVisible(false);
         mainNav.style.backgroundColor = 'transparent';
+        setNavbar(false);
     };
 
     return (
         <>
             <Container className='HeaderContainer' onMouseLeave={handleMouseLeave}>
+
+
                 <a>
                     <svg viewBox="0 0 342 35" className="teslalogo">
-                        <path className="teslalogo" d="M0 .1a9.7 9.7 0 0 0 7 7h11l.5.1v27.6h6.8V7.3L26 7h11a9.8 9.8 0 0 0 7-7H0zm238.6 0h-6.8v34.8H263a9.7 9.7 0 0 0 6-6.8h-30.3V0zm-52.3 6.8c3.6-1 6.6-3.8 7.4-6.9l-38.1.1v20.6h31.1v7.2h-24.4a13.6 13.6 0 0 0-8.7 7h39.9v-21h-31.2v-7h24zm116.2 28h6.7v-14h24.6v14h6.7v-21h-38zM85.3 7h26a9.6 9.6 0 0 0 7.1-7H78.3a9.6 9.6 0 0 0 7 7zm0 13.8h26a9.6 9.6 0 0 0 7.1-7H78.3a9.6 9.6 0 0 0 7 7zm0 14.1h26a9.6 9.6 0 0 0 7.1-7H78.3a9.6 9.6 0 0 0 7 7zM308.5 7h26a9.6 9.6 0 0 0 7-7h-40a9.6 9.6 0 0 0 7 7z" fill="currentColor" className={navbar ? 'navbar' : 'navbarActive'}>
-
+                        <path d="M0 .1a9.7 9.7 0 0 0 7 7h11l.5.1v27.6h6.8V7.3L26 7h11a9.8 9.8 0 0 0 7-7H0zm238.6 0h-6.8v34.8H263a9.7 9.7 0 0 0 6-6.8h-30.3V0zm-52.3 6.8c3.6-1 6.6-3.8 7.4-6.9l-38.1.1v20.6h31.1v7.2h-24.4a13.6 13.6 0 0 0-8.7 7h39.9v-21h-31.2v-7h24zm116.2 28h6.7v-14h24.6v14h6.7v-21h-38zM85.3 7h26a9.6 9.6 0 0 0 7.1-7H78.3a9.6 9.6 0 0 0 7 7zm0 13.8h26a9.6 9.6 0 0 0 7.1-7H78.3a9.6 9.6 0 0 0 7 7zm0 14.1h26a9.6 9.6 0 0 0 7.1-7H78.3a9.6 9.6 0 0 0 7 7zM308.5 7h26a9.6 9.6 0 0 0 7-7h-40a9.6 9.6 0 0 0 7 7z" fill="currentColor"
+                            className={navbar ? 'navbar teslalogo' : 'navbarActive teslalogo'}>
                         </path>
                     </svg><span className="t-logo">Tesla</span>
                 </a>
@@ -137,6 +139,11 @@ const Header = () => {
         </>
 
     )
+
+
+
+
+
 }
 
 export default Header
@@ -158,11 +165,15 @@ const Menu = styled.div`
     justify-content:center;
     flex:1;
     a{
-        font-weight:600;
+        font-weight:500;
         text-transform:uppercase;
-        padding:0 10px;
+        padding: 10px;
         flex-wrap: nowrap;
+        border-radius:5px;
     }
+    :hover {
+        background-color:#F7F9F9;
+     }
     @media(max-width:768px){
         display:none;
     }
